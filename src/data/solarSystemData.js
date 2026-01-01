5be129960c1003d06f3f9ebdaf99f1eb5d28f3eb
LAPTOP-P8J5SK19\Woody <woodylin1024@gmail.com>
2026-01-01 10:26:07 +0800

    Fix: Replace problematic Io texture with a shadows-free generated map

--- src/data/solarSystemData.js
+++ src/data/solarSystemData.js
@@ -153,7 +153,7 @@ export const solarSystemData = [
                 radius: 0.28,
                 realScaleRadius: 0.285,
                 color: 0xfff000,
-                texture: "io.jpg",
+                texture: "io.png",
                 distance: 5,
                 realScaleDistance: 13,
                 speed: 0.04,
